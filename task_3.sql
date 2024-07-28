9f573eab7e6a51cba8873f7a5b440955392b7159
KON-R4D <tyanlearner@gmail.com>
2024-07-28 21:23:42 +0300

    modified task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,4 +1,7 @@
 -- List all tables in the specified database
-SELECT table_name
-FROM information_schema.tables
-WHERE table_schema = DATABASE();
+-- SELECT table_name
+-- FROM information_schema.tables
+-- WHERE table_schema = DATABASE();
+
+-- List all tables in the specified database
+SHOW TABLES;
